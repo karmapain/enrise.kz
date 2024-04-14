40b63be55f2c2fc69f07cdc550192487adffaad5
sultanalikhan1 <sultan.alikhan@mail.ru>
2024-04-15 02:06:14 +0600

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -1,9 +1,7 @@
 <?php
-
 require_once __DIR__ . '/vendor/autoload.php';
 $settings = require_once __DIR__ . '/settings.php';
 require_once __DIR__ . '/functions.php';
-
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Получаем данные из POST-запроса
@@ -12,23 +10,25 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $name = $_POST['name'];
     $message = $_POST['message'];
 
+    if(!empty($message)){
+        $body = "<h1>Письмо с сайта. Оставлена заявка пользователем :&nbsp;" . $name . "</h1><p>\n
+        ФИО:&nbsp;" . $name .
+        "\nНомер телефона:&nbsp;" . $phone .
+        "\nПочта:&nbsp;" . $email .
+        "\nСопроводительный текст:&nbsp;" . $message . "</p>";
+    }else{
+        $body = "<h1>Письмо с сайта. Оставлена заявка пользователем :&nbsp;" . $name . "</h1><p>\n
+        ФИО:&nbsp;" . $name .
+        "\nНомер телефона:&nbsp;" . $phone .
+        "\nПочта:&nbsp;" . $email . "</p>";
+    }
 
-    $body = "<h1>Письмо с сайта. Оставлена заявка пользователем :&nbsp;" . $name . "</h1>\n
-    ФИО:&nbsp;" . $name .
-    "\nНомер телефона:&nbsp;" . $phone .
-    "\nПочта:&nbsp;" . $email .
-    "\nСопроводительный текст:&nbsp;" . $message;
-
-    /*$attachments = [
-        __DIR__ . '/images/4.jpg',
-        __DIR__ . '/images/5.jpg',
-    ];*/
-
-    /*var_dump(send_mail($settings['mail_settings_prod'], ['sultanalikhan61@gmail.com', 'alimdosmatov@gmail.com'], 'Письмо с сайта', $body, $attachments));*/
-
-    var_dump(send_mail($settings['mail_settings_prod'], ['sultanalikhan61@gmail.com', 'alimdosmatov@gmail.com'], 'Письмо с сайта', $body));
+    if(send_mail($settings['mail_settings_prod'], ['sultanalikhan61@gmail.com', 'alimdosmatov@gmail.com'], 'Письмо с сайта', $body)){
+        // Если отправка успешна, выводим JavaScript с alert и перенаправлением
+        echo '<script>alert("Письмо успешно отправлено"); window.location.href = "index.html";</script>';
+    }else{
+        // Если отправка не удалась, выводим JavaScript с alert и перенаправлением
+        echo '<script>alert("Ошибка при отправке письма"); window.location.href = "index.html";</script>';
+    }
 }
-
-
 ?>
-
